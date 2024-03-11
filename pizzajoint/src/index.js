224b9d62ace726c5a306b38d636364c577769524
Grishon Nganga <grishon.nganga01@gmail.com>
2024-03-12 00:57:34 +0300

    Just learning framer-motion.

--- pizzajoint/src/index.js
+++ pizzajoint/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import "@fontsource/quicksand"; 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
 );
+
+
